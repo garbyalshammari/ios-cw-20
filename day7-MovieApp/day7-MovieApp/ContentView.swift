21e5dae3c43efaed3546d23b2f7d0b1977608dc5
غربي الشمري <100927646+garbyalshammari@users.noreply.github.com>
2022-03-23 18:22:06 +0300

    Done

--- day7-MovieApp/day7-MovieApp/ContentView.swift
+++ day7-MovieApp/day7-MovieApp/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    let movies = ["Dolittle", "Morbius" ,"Spider man"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(movies, id: \.self){moviee in movieRow(movie2: moviee)
+            
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+struct movieRow: View {
+    var movie2 : String
+    
+    
+    var body: some View {
+        HStack{
+           Image(movie2)
+                .resizable()
+                .scaledToFit()
+                .frame(height:120)
+                .clipShape(Circle())
+            
+            
+            
+            Text(movie2)
+                .font(.headline)
+                .fontWeight(.bold)
+            
+            
+            
+        }
+        
     }
 }
